2d50f4ff43851d6b0e99167c0866e0a7449f4ae1
Riya Yadav <114867989+ry129@users.noreply.github.com>
2025-01-18 16:31:39 +0530

    Update code5.java

--- code5.java
+++ code5.java
@@ -1,25 +1,4 @@
-import java.util.*;
-class Main{
-	public static void check(String s1 , String s2){
-		int count=0;
-		
-		for(int i=0;i<s1.length();i++){	
-			for(int j=i;j<=i;j++){
-			String subs= s1.substring(i+" "+j);	
-			count++;
-			}
-		}
-		return count;
-		return subs;
-	}
-   public static void main(String[]args){
-		Scanner sc=new Scanner(System.in);
-		String s1=sc.nextLine();
-		String s2=sc.nextLine();
-	System.out.println(check(s1,s2));
-		
-	}
-}
+
 			
 			
 			
